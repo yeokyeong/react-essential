3ef65155dd6a73ecf9157ec65d6a6aa8766f1d01
= <yeokyeong1004@gmail.com>
2024-02-25 17:26:45 +0000

    created GameOver & added winning logic

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
-import { useState } from "react";
+import GameOver from "./components/GameOver";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveCurrentPlayer(turns) {
   let currentPlayer = "X";
@@ -13,16 +21,49 @@ function deriveCurrentPlayer(turns) {
 }
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+
   const currentPlayer = deriveCurrentPlayer(gameTurns);
+  const gameBoard = [...initialGameBoard.map((inner) => [...inner])];
+
+  for (const turns of gameTurns) {
+    // it will excute whenever App component render
+    const { square, player } = turns;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+  for (const combination of WINNING_COMBINATIONS) {
+    // it will excute whenever App component render
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIdx, colIdx) {
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveCurrentPlayer(prevTurns);
-
       return [
         { square: { row: rowIdx, col: colIdx }, player: currentPlayer },
         ...prevTurns,
       ];
     });
+  }
+
+  function handleRestart() {
+    setGameTurns([]);
   }
   return (
     <>
@@ -32,9 +73,12 @@ function App() {
           <Player name="player1" symbol="X" isActive={currentPlayer === "X"} />
           <Player name="player2" symbol="0" isActive={currentPlayer === "O"} />
         </ol>
+        {(hasDraw || winner) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
         <GameBoard
           handleSelectSquare={handleSelectSquare}
-          gameTurns={gameTurns}
+          gameBoard={gameBoard}
         />
       </main>
       <Log gameTurns={gameTurns} />
--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,19 +1,4 @@
-import { useState } from "react";
-
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ handleSelectSquare, gameTurns }) {
-  const gameBoard = [...initialGameBoard];
-  for (const turns of gameTurns) {
-    const { square, player } = turns;
-    const { row, col } = square;
-    gameBoard[row][col] = player;
-  }
-
+export default function GameBoard({ handleSelectSquare, gameBoard }) {
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIdx) => (
@@ -21,7 +6,10 @@ export default function GameBoard({ handleSelectSquare, gameTurns }) {
           <ol>
             {row.map((playerSymbol, colIdx) => (
               <li key={colIdx}>
-                <button onClick={() => handleSelectSquare(rowIdx, colIdx)}>
+                <button
+                  onClick={() => handleSelectSquare(rowIdx, colIdx)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
